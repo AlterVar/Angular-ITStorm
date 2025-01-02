23c41699a4e50624fca738f1498d18c728df2a94
Alexandra Kolpakova <alexandra.k.w@yandex.ru>
2025-01-02 17:00:51 +0600

    feat(footer): add popup request to footer

--- frontend/src/app/shared/layout/footer/footer.component.ts
+++ frontend/src/app/shared/layout/footer/footer.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {DefaultResponseType} from "../../../../types/default-response.type";
+import {FormBuilder, Validators} from "@angular/forms";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {CallbackService} from "../../services/callback.service";
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +10,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  dialog: HTMLElement | null = null;
+  dialogBtn: HTMLElement | null = null;
+  requestIsSent: boolean = false;
 
-  constructor() { }
+  consultationForm = this.fb.group({
+    name: ['', Validators.required],
+    phone: ['', [Validators.required, Validators.pattern(/^[0-9]*$/)]]
+  })
+
+  constructor(private _snackBar: MatSnackBar,
+              private callbackService: CallbackService,
+              private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.dialog = document.getElementById('consultation-dialog');
+    this.dialogBtn = document.getElementById('consultation-btn');
   }
 
+  openDialog() {
+    if (this.dialog) {
+      this.dialog.style.display = 'flex';
+    }
+  }
+
+  closeDialog(event: Event) {
+    event.stopPropagation();
+    this.requestIsSent = false;
+    if (this.dialog && (event.target === event.currentTarget || event.currentTarget === this.dialogBtn)) {
+      this.dialog.style.display = 'none';
+    }
+  }
+
+  //TODO: возможно использовать таки AngularMaterials или для попапа отдельный компонент использовать
+  sendCallbackRequest() {
+    if (this.consultationForm.value.name && this.consultationForm.value.phone) {
+      this.callbackService.sendCallbackRequest({
+        name: this.consultationForm.value.name,
+        phone: this.consultationForm.value.phone,
+        type: "consultation"
+      })
+        .subscribe((data: DefaultResponseType) => {
+          if (data.error) {
+            this._snackBar.open('Не удалось отправить запрос. Пожалуйста, обратитесь в службу поддержки');
+            console.log(data.message);
+          }
+          this.requestIsSent = true;
+        })
+    }
+  }
 }
--- frontend/src/types/callback.type.ts
+++ frontend/src/types/callback.type.ts
@@ -1,6 +1,6 @@
 export type CallbackType = {
   name: string,
   phone: string,
-  service: string,
+  service?: string,
   type: string
 }
